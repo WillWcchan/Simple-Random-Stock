e795e909fe57db9e532538b399465619a628ab9a
WillWcchan <WillWcchan@gmail.com>
2020-07-12 21:29:57 -0700

    Allow email to be sent

--- app/src/main/java/com/willchan/simple_random_stock/activities/AboutActivity.java
+++ app/src/main/java/com/willchan/simple_random_stock/activities/AboutActivity.java
@@ -1,6 +1,11 @@
 package com.willchan.simple_random_stock.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,11 @@ import androidx.appcompat.widget.Toolbar;
 import com.willchan.simple_random_stock.R;
 
 public class AboutActivity extends AppCompatActivity {
-
+    private final String MAILTO = "mailto:";
+    private final String TYPE = "text/plain";
+    private final String SUBJECT = "Greetings Will from Simple-Random-Stock";
+    private final String CHOOSE_MAIL_APP = "Choose Mail App";
+    private final String NO_EMAIL_CLIENT_INSTALLED = "There is no email client installed.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,26 @@ public class AboutActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
 
         // Enable the Up Button
-        ab.setDisplayHomeAsUpEnabled(true);
+        if (ab != null) {
+            try {
+                ab.setDisplayHomeAsUpEnabled(true);
+            } catch (NullPointerException e) {
+                Log.e(AboutActivity.this.getLocalClassName(), "Unable to set Up Action");
+            }
+        }
     }
 
-
+    // Source: https://www.tutorialspoint.com/android/android_sending_email.htm#:~:text=Intent%20Object%20-%20Extra%20to%20send%20Email%20,that%20i%20...%20%203%20more%20rows%20
+    // Email link in the About Activity was clicked
+    public void sendEmail(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setDataAndType(Uri.parse(MAILTO), TYPE);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
+        try {
+            startActivity(Intent.createChooser(emailIntent, CHOOSE_MAIL_APP));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, NO_EMAIL_CLIENT_INSTALLED, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
